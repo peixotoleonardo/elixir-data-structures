c33d673ff8719a033e87bde5df9e34064c0b1eb6
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2024-09-01 11:03:04 -0300

    chore: init project

---
+++ lib/elixir_data_structures.ex
@@ -0,0 +1,18 @@
+defmodule ElixirDataStructures do
+  @moduledoc """
+  Documentation for `ElixirDataStructures`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirDataStructures.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_data_structures_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirDataStructuresTest do
+  use ExUnit.Case
+  doctest ElixirDataStructures
+
+  test "greets the world" do
+    assert ElixirDataStructures.hello() == :world
+  end
+end
